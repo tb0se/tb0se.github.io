ff5a44377a09b30bec06d1842546224d295ad5d8
tb0se <rachiditbose@gmail.com>
2024-04-27 23:10:38 +0200

    Update about

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,9 @@ export default defineConfig({
   site: 'https://tb0se.github.io',
   integrations: [tailwind(), icon({
     include: {
-      logos: ['github-icon',],
+      logos: ['github-icon', 'javascript', 'typescript-icon', 'python', 'react', 'solidjs-icon',
+        'svelte-icon', 'nodejs-icon', 'tailwindcss-icon', 'html-5', 'postgresql', 'bun', 'astro-icon',
+        'css-3', 'docker-icon', 'jira', 'aws', 'confluence', 'vitejs', 'ubuntu', 'github-actions', 'github'],
       heroicons: ['bars-3-solid']
     }
   })]
