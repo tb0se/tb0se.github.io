a71f0a8640756bc6d5710e69e032bd8ba8ca120b
tb0se <rachiditbose@gmail.com>
2024-05-01 17:46:57 +0200

    Add projects

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,6 @@
-export const githubUrl = 'https://github.com/tb0se';
+export const personalGithubUrl = 'https://github.com/tb0se';
+
+export const workGithubUrl = 'https://github.com/thaboRach';
 
 export const astroUrl = 'https://astro.build/';
 
